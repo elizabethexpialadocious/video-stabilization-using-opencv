85169a80bbb0bb7ef06adb414de71a6b51457fa1
elizabethexpialadocious <sunshinekisses@gmail.com>
2023-08-08 11:25:04 -0600

    added support for imShow, problems with CLI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,30 @@
-import org.bytedeco.javacpp.indexer.DoubleIndexer
-import org.bytedeco.javacpp.indexer.Indexer
 import org.bytedeco.javacpp.indexer.ShortIndexer
-import org.bytedeco.javacpp.indexer.UByteIndexer
 import org.bytedeco.opencv.global.opencv_core.CV_16S
+import org.bytedeco.opencv.global.opencv_highgui.waitKey
 import org.bytedeco.opencv.opencv_core.Mat
 import org.bytedeco.opencv.opencv_videoio.VideoWriter
 import org.bytedeco.opencv.opencv_videostab.IFrameSource
 
 
 
+
+
+
+
 var stabilizedFrames = IFrameSource()
 var outputPath = String()
 var outputFps : Double = 32.0
+var quietMode = false
 
 fun run() {
     val writer = VideoWriter()
-    var stabilizedFrame: Mat = Mat()
-    var nframes: Int = 0;
+    var stabilizedFrame: Mat
+    var nframes: Int = 0
     var fileName: String
+    val imshow : ImShow? = when {
+        !quietMode -> ImShow("stabilizedFrame")
+        else -> null
+    }
 
     stabilizedFrame = stabilizedFrames.nextFrame()
     while (!stabilizedFrame.empty()) {
@@ -37,11 +44,31 @@ fun run() {
             writer.write(stabilizedFrame)
         }
 
+        if (!quietMode) {
+            /*
+            val conv1 = OpenCVFrameConverter.ToMat()
+            val conv2 = OpenCVFrameConverter.ToOrgOpenCvCoreMat()
+            val cvmat : org.opencv.core.Mat = conv2.convert(conv1.convert(stabilizedFrame))
+             */
 
+            imshow!!.showImage(stabilizedFrame)
 
+            /*
+                sprintf(file_name, "%0.3d.tif", nframes);
+                imwrite(file_name, stabilizedFrame);
+            */
 
+            val k = waitKey(3)
+            if (k == 27) {
+                // close file?
+                println()
+                break
+            }
+        }
         stabilizedFrame = stabilizedFrames.nextFrame()
     }
+    println("processed frames: $nframes")
+    println("finished")
 }
 
 fun dumpMat(m : Mat) {
@@ -57,6 +84,7 @@ fun dumpMat(m : Mat) {
 
 fun main(args: Array<String>) {
     println("Hello World!")
+    // val cmd = ArgParser()
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
@@ -65,5 +93,14 @@ fun main(args: Array<String>) {
     val m : Mat? = Mat.eye(5, 5, CV_16S).asMat()
 
     dumpMat(m!!)
+
+
+
+
+}
+
+
+
+public interface IMotionEstimatorBuilder {
 
 }
